74c5c1f6533d8e41d88df60c1130a8aed866faac
Smruti <63377540+smruti943@users.noreply.github.com>
2024-08-25 19:54:42 +0200

    DB password updated for develop branch ..

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password', //database password
+    password: 'Zaq1@wsx', //database password
     database: 'auth_db'   // database name
 });
 
